7a7e2fd91824b8b19b8202e9092e65f41838c410
hughmalan <hughmalan@gmail.com>
2018-07-25 22:12:32 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,54 +8,90 @@
 
 'use strict';
 
-const snapshotButton = document.querySelector('button#snapshot');
-const filterSelect = document.querySelector('select#filter');
+//const filterSelect = document.querySelector('select#filter');
 
 // Put variables in global scope to make them available to the browser console.
 const video = window.video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
+const button = document.getElementById('button');
+const select = document.getElementById('select');
+
 canvas.width = 480;
 canvas.height = 360;
 
-snapshotButton.onclick = function() {
-  canvas.className = filterSelect.value;
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-};
-
+/*
 filterSelect.onchange = function() {
   video.className = filterSelect.value;
 };
 
-const constraints = {
-  audio: false,
-  video: true
-};
+	const constraints = {
+	  audio: false,
+	  video: true
+	};
 
-function handleSuccess(stream) {
-  window.stream = stream; // make stream available to browser console
-  video.srcObject = stream;
+	function handleSuccess(stream) {
+	  window.stream = stream; // make stream available to browser console
+	  video.srcObject = stream;
+	}
+
+	function handleError(error) {
+	  console.log('navigator.getUserMedia error: ', error);
+	}
+
+	navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+*/
+
+
+
+let currentStream;
+
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach(track => {
+    track.stop();
+  });
 }
 
-function handleError(error) {
-  console.log('navigator.getUserMedia error: ', error);
+function gotDevices(mediaDevices) {
+  select.innerHTML = '';
+  select.appendChild(document.createElement('option'));
+  let count = 1;
+  mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  });
 }
-
-navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
-
-
-
 
 button.addEventListener('click', event => {
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream);
+  }
+  const videoConstraints = {};
+  if (select.value === '') {
+    videoConstraints.facingMode = 'environment';
+  } else {
+    videoConstraints.deviceId = { exact: select.value };
+  }
   const constraints = {
-    video: true,
+    video: videoConstraints,
     audio: false
   };
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(stream => {
+      currentStream = stream;
       video.srcObject = stream;
+      return navigator.mediaDevices.enumerateDevices();
     })
+    .then(gotDevices)
     .catch(error => {
       console.error(error);
     });
 });
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
